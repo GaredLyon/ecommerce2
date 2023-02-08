16d0ae2e3dd9c6ce562858eff8882eaf47c3545e
jaen <jaenfigueroa03042000@gmail.com>
2023-02-07 22:04:08 -0500

    css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import './Header.css'
 
 export const Header = () => {
   return (
-    <header className='header'>Header</header>
+    <header className='header'>
+      <div className='header__logo'>
+        <p>Footcap</p>
+      </div>
+      <nav className='header__nav'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Products</li>
+          <li>Shop</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
+      <ul className='header__iconos'>
+        <li>x</li>
+        <li>x</li>
+        <li>x</li>
+        <li>x</li>
+      </ul>
+
+    </header>
   )
 }
